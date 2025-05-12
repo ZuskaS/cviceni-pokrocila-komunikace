c444e594f74da01ab44a05c3ff574c2fa661e013
Zuzana Ilečková <yes>
2025-05-12 22:04:33 +0200

    splnění druhého úkolu

--- src/components/Ukol2/CoinButton/index.jsx
+++ src/components/Ukol2/CoinButton/index.jsx
@@ -1,8 +1,14 @@
 import './style.css';
+import { useState } from 'react';
 
-export const CoinButton = ({ value }) => {
+export const CoinButton = ({ value, onCoinClick }) => {
   return (
-    <button className="icon-button">
+    <button
+      className="icon-button"
+      onClick={() => {
+        onCoinClick(value);
+      }}
+    >
       <i className="plus-icon" />
       <div className="button-label">{value}</div>
     </button>
