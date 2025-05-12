1021ab8dda06f1dfd9fcc06970276d28a292a660
Zuzana Ilečková <yes>
2025-05-12 22:29:25 +0200

    splnění třetího úkolu

--- src/components/Ukol3/Product/index.jsx
+++ src/components/Ukol3/Product/index.jsx
@@ -1,12 +1,21 @@
 import './style.css';
+import { useState } from 'react';
 
-export const Product = ({ image, name, price }) => {
+export const Product = ({ image, name, price, onAddToCart }) => {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="product">
+    <div
+      className="product"
+      onClick={() => {
+        onAddToCart(price);
+        setCount(count + 1);
+      }}
+    >
       <img src={image} className="product__image" />
       <div className="product__body">
         {name}: {price} Kč <br />
-        0 ks
+        {count} ks
       </div>
     </div>
   );
